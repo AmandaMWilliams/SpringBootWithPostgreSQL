484b3cac14dd7c1dd05affa668028b3396cf3b17
AmandaMWilliams <amandawilliams1915@gmail.com>
2021-03-03 13:36:21 -0500

    Added first student to main

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,15 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -13,7 +19,16 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	@GetMapping
-	public String hello(){
-		return "Hello World";
+	public List<Student> hello(){
+		List studentList = new ArrayList();
+		Student amanda = new Student(
+				1L,
+				"Amanda",
+				"amanda@github.com",
+				LocalDate.of(1988, Month.DECEMBER, 19),
+				32
+		);
+		studentList.add(amanda);
+		return studentList;
 	}
 }
